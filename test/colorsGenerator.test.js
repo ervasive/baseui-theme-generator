4973ff13d367fe752270780c6c2a9645e651fca1
Ruslan Vasiliev <ervasive@gmail.com>
2019-04-18 17:08:46 +0300

    Reformat colorsGenerator.test.js

--- test/colorsGenerator.test.js
+++ test/colorsGenerator.test.js
@@ -1,71 +1,71 @@
-import { createSetColor } from "../src/colorsGenerator";
+import {createSetColor} from '../src/colorsGenerator'
 
-describe("Colors generator", () => {
-  it("should return light color primitive", () => {
+describe('Colors generator', () => {
+  it('should return light color primitive', () => {
     const setColor = createSetColor(
       {
-        lightPrimitive: "lightPrimitiveValue",
-        darkPrimitive: "darkPrimitiveValue"
+        lightPrimitive: 'lightPrimitiveValue',
+        darkPrimitive: 'darkPrimitiveValue',
       },
-      "light"
-    );
+      'light',
+    )
 
-    expect(setColor("lightPrimitive", "darkPrimitive")).toEqual(
-      "lightPrimitiveValue"
-    );
-  });
+    expect(setColor('lightPrimitive', 'darkPrimitive')).toEqual(
+      'lightPrimitiveValue',
+    )
+  })
 
-  it("should return dark color primitive", () => {
+  it('should return dark color primitive', () => {
     const setColor = createSetColor(
       {
-        lightPrimitive: "lightPrimitiveValue",
-        darkPrimitive: "darkPrimitiveValue"
+        lightPrimitive: 'lightPrimitiveValue',
+        darkPrimitive: 'darkPrimitiveValue',
       },
-      "dark"
-    );
+      'dark',
+    )
 
-    expect(setColor("lightPrimitive", "darkPrimitive")).toEqual(
-      "darkPrimitiveValue"
-    );
-  });
+    expect(setColor('lightPrimitive', 'darkPrimitive')).toEqual(
+      'darkPrimitiveValue',
+    )
+  })
 
-  it("should return light color primitive if dark variant is missing", () => {
+  it('should return light color primitive if dark variant is missing', () => {
     const setColor = createSetColor(
       {
-        lightPrimitive: "lightPrimitiveValue",
-        darkPrimitive: "darkPrimitiveValue"
+        lightPrimitive: 'lightPrimitiveValue',
+        darkPrimitive: 'darkPrimitiveValue',
       },
-      "dark"
-    );
+      'dark',
+    )
 
-    expect(setColor("lightPrimitive")).toEqual("lightPrimitiveValue");
-  });
+    expect(setColor('lightPrimitive')).toEqual('lightPrimitiveValue')
+  })
 
-  it("should pass through the provided light value if the primitives object does not contain it", () => {
+  it('should pass through the provided light value if the primitives object does not contain it', () => {
     const setColor = createSetColor(
       {
-        lightPrimitive: "lightPrimitiveValue",
-        darkPrimitive: "darkPrimitiveValue"
+        lightPrimitive: 'lightPrimitiveValue',
+        darkPrimitive: 'darkPrimitiveValue',
       },
-      "light"
-    );
+      'light',
+    )
 
     expect(
-      setColor("non-existent-value-light", "non-existent-value-dark")
-    ).toEqual("non-existent-value-light");
-  });
+      setColor('non-existent-value-light', 'non-existent-value-dark'),
+    ).toEqual('non-existent-value-light')
+  })
 
-  it("should pass through the provided dark value if the primitives object does not contain it", () => {
+  it('should pass through the provided dark value if the primitives object does not contain it', () => {
     const setColor = createSetColor(
       {
-        lightPrimitive: "lightPrimitiveValue",
-        darkPrimitive: "darkPrimitiveValue"
+        lightPrimitive: 'lightPrimitiveValue',
+        darkPrimitive: 'darkPrimitiveValue',
       },
-      "dark"
-    );
+      'dark',
+    )
 
     expect(
-      setColor("non-existent-value-light", "non-existent-value-dark")
-    ).toEqual("non-existent-value-dark");
-  });
-});
+      setColor('non-existent-value-light', 'non-existent-value-dark'),
+    ).toEqual('non-existent-value-dark')
+  })
+})
